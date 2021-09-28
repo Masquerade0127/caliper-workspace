93a8ef207801fa9b3ca6e4d1b0c16c3f11e5781e
masquerade0127 <masquerade236@gmail.com>
2021-09-28 18:50:38 +0700

    update benchmarks script

---
+++ bloody-benchmarks.sh
@@ -0,0 +1,1 @@
+npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/networkConfig.yaml --caliper-benchconfig benchmarks/myAssetBenchmark.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled
